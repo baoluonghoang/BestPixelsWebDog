5c817f78aa67dcd963b4a3930a78844f81f7d477
baolh <baolh@runsystem.net>
2020-12-18 16:49:54 -0800

    new dog web

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const menu = document.querySelector(".menu");
+const menuOpen = document.querySelector(".mini-menu");
+const menuClose = document.querySelector(".close");
+const navbar = document.querySelector("#header");
+
+const navLeft = menu.getBoundingClientRect().left;
+
+menuOpen.addEventListener('click', () => {
+    if(navLeft < 0) {
+        menu.classList.add("show");
+        document.body.classList.add("show");
+        navbar.classList.add("show");
+    }
+    console.log('in');
+});
+
+menuClose.addEventListener('click', () => {
+    if(navLeft < 0) {
+        menu.classList.remove("show");
+        document.body.classList.remove("show");
+        navBar.classList.remove("show");
+    }
+
+    console.log('out');
+});
